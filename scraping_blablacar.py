6f3bff040553336e6ae893b0274f018a96b7f6e4
psaffers <32955015+psaffers@users.noreply.github.com>
2017-11-03 13:42:41 +0100

    Automatisation + Gmaps
    Automatisation du parcours de tous les trajets principaux
    Une fois les lieux d'arrivée et départ repérés, programme de géocoding avec gmaps pour récupérer les coordonnées

--- scraping_blablacar.py
+++ scraping_blablacar.py
@@ -4,18 +4,26 @@ Created on Mon Oct 23 12:54:59 2017
 
 @author: psaffers
 """
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
 from datetime import date
+import sys
+sys.path.append("c:/users/psaffers/appdata/local/programs/python/python35/lib/site-packages")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import googlemaps
+import json
 
 #Définition des constantes de date
 today = date.today()
 tomor = date.today()+datetime.timedelta(1)
-j_j   = str(today.year)+"-"+str(today.month)+"-"+str(today.day)
-j_p_1 = str(tomor.year)+"-"+str(tomor.month)+"-"+str(tomor.day)
+j_j   = str(today.year)+"-"+'0'*(1-len(str(today.month)))+str(today.month)+"-"+'0'*(2-len(str(today.day)))+str(today.day)
+j_p_1 = str(tomor.year)+"-"+'0'*(1-len(str(tomor.month)))+str(tomor.month)+"-"+'0'*(2-len(str(tomor.day)))+str(tomor.day)
 
 #On récupère la structure de la page
 page = requests.get("https://www.blablacar.fr", headers={'User-Agent': 'Mozilla/5.0'})
@@ -61,59 +69,89 @@ for j in range(0,len(secondaire)):
 trajets_table = pd.DataFrame({'url':http,'start':dep,'end':arr})
 trajets_table = trajets_table[(trajets_table['end']=='Paris')|(trajets_table['start']=='Paris')].reset_index()[['start','end','url']]
 
-
-###
-#On parcourt alors chacun des tops trajets définis
-###
-page_temp = requests.get("https://www.blablacar.fr"+trajets_table['url'][0], 
-                          headers={'User-Agent': 'Mozilla/5.0'})
-#Initialisation des variables
-if trajets_table['start'][0]=='Paris':
-    type= 'start'
-else:
-    type= 'end'
-round     = 0
-stop_crit = 0
-start_temp=[]
-end_temp  =[]
-date_temp =[]
-type_temp =[]
-
-while stop_crit!=1:
-    print(round)
-    if round!=0:
-        page_temp = requests.get("https://www.blablacar.fr"+url_temp, 
-                                  headers={'User-Agent': 'Mozilla/5.0'})
-    soup_temp = BeautifulSoup(page_temp.content, 'html.parser')
-    dep_arr_t = soup_temp.findAll("dd")
-    date_dep_ = soup_temp.findAll("h3", attrs={'class':'time u-darkGray'})
-    for k in range(0,len(dep_arr_t)):
-        bal_temp = soup_temp.findAll("dd")[k]
-        if bal_temp.get("title")=='Départ':
-            start_temp.append(bal_temp.getText().replace('\n','').strip())
-        if bal_temp.get('title')=='Arrivée':
-            end_temp.append(bal_temp.getText().replace('\n','').strip())
-        if k%2==0:
-            date_temp.append(date_dep_[int(k/2)].get("content"))
-            print(date_dep_[int(k/2)].get("content"))
-            if ((date_dep_[int(k/2)].get("content")!=j_p_1)&(date_dep_[int(k/2)].get("content")!=j_j)):
-                stop_crit=1
-            type_temp.append(type)
-    #On récupère le lien de la page suivante
-    #1 - On repère le lien de la page active
-    p_act = int(soup_temp.findAll("li",attrs={'class':'active'})[0].getText())
-    #2 - On récupère l'ensemble des liens de pagination avec la valeur de page associée
-    p_oth = soup_temp.findAll("a",attrs={'class':'js-trip-search-pagination'})
-    for l in range(0,len(p_oth)):
-        try:
-            if int(p_oth[l].getText())==p_act+1:
-                url_temp = p_oth[l].get('href')
-        except:
-            continue
-    round=round+1
+#On définit le webdriver
+driver = webdriver.Chrome('C:/Program Files (x86)/Google/Chrome/chromedriver')
+#On définit notre future table d'étude
+covoit = pd.DataFrame(columns=['date','type','place'])
+#On itère sur les différentes URL trouvées
+for i in range(0,len(trajets_table['url'])):
+    #On récupère le code source de la page principale
+    driver.get("https://www.blablacar.fr"+trajets_table['url'][i])
+    #Initialisation des variables
+    if trajets_table['start'][i]=='Paris':
+        type= 'start'
+    else:
+        type= 'end'
+    round     = 0
+    stop_crit = 0
+    start_temp=[]
+    end_temp  =[]
+    date_temp =[]
+    type_temp =[]
     
-print(start_temp)
+    while stop_crit!=1:
+        print(round)
+        if round!=0:
+            driver.get("https://www.blablacar.fr"+url_temp)
+            time.sleep(5)
+            #try:
+             #   element = WebDriverWait(driver, 10).until(
+              #          EC.presence_of_element_located((By.ID, "search-results"))
+               #         )
+            #except TimeoutException:
+             #   print('Page not reachable')
+        soup_temp = BeautifulSoup(driver.page_source, 'html.parser')
+        dep_arr_t = soup_temp.findAll("dd")
+        date_dep_ = soup_temp.findAll("h3", attrs={'class':'time u-darkGray'})
+        for k in range(0,len(dep_arr_t)):
+            bal_temp = soup_temp.findAll("dd")[k]
+            if bal_temp.get("oldtitle")=='Départ':
+                start_temp.append(bal_temp.getText().replace('\n','').strip())
+            if bal_temp.get('oldtitle')=='Arrivée':
+                end_temp.append(bal_temp.getText().replace('\n','').strip())
+            if k%2==0:
+                date_temp.append(date_dep_[int(k/2)].get("content"))
+                if ((date_dep_[int(k/2)].get("content")!=j_p_1)&(date_dep_[int(k/2)].get("content")!=j_j)):
+                    stop_crit=1
+                type_temp.append(type)
+        #On récupère le lien de la page suivante
+        #1 - On repère le lien de la page active
+        p_act = int(soup_temp.findAll("li",attrs={'class':'active'})[0].getText())
+        #2 - On récupère l'ensemble des liens de pagination avec la valeur de page associée
+        p_oth = soup_temp.findAll("a",attrs={'class':'js-trip-search-pagination'})
+        for l in range(0,len(p_oth)):
+            try:
+                if int(p_oth[l].getText())==p_act+1:
+                    url_temp = p_oth[l].get('href').replace('limit=10','limit=100')
+            except:
+                continue
+        round=round+1
+    
+    if type=='start':
+        covoit_temp = pd.DataFrame({'date':date_temp,'type':type_temp,'place':start_temp})[['date','type','place']]
+    elif type=='end':
+        covoit_temp = pd.DataFrame({'date':date_temp,'type':type_temp,'place':end_temp})[['date','type','place']]
+    
+    covoit = pd.concat([covoit,covoit_temp])
 
-page_tst = requests.get("https://www.blablacar.fr/#?fn=Paris&fc=48.856614%7C2.3522219&fcc=FR&fp=0&tn=Lyon&tc=45.764043%7C4.835659&tcc=FR&tp=0&sort=trip_date&order=asc&radius=40.149&limit=10&page=1", 
-                                  headers={'User-Agent': 'Mozilla/5.0'})
-page_tst.content
+driver.quit()
+del type
+#Dédoublonnage et filtre sur la table 'covoit'
+covoit_v1 = covoit.drop_duplicates()[(covoit.drop_duplicates()['date']==j_j)|(covoit.drop_duplicates()['date']==j_p_1)].reset_index()[['date','type','place']]
+
+#On recherche les coordonnées GPS des départs et arrivés
+gmaps = googlemaps.Client(key='AIzaSyBhDB0PdRyo2dVpbZxvB5c9n5egLmlkJ1Y')
+#On géocode les lieux trouvés
+covoit_v1['lat']=['' for place in covoit_v1['place']]
+covoit_v1['lng']=covoit_v1['lat']
+for l in range(0,len(covoit_v1['place'])):
+    try:
+        geocode_result      = gmaps.geocode(covoit_v1['place'][l]).pop()
+        covoit_v1['lat'][l] = geocode_result['geometry']['location']['lat']
+        print(geocode_result['geometry']['location']['lat'])
+        covoit_v1['lng'][l] = geocode_result['geometry']['location']['lng']
+        time.sleep(0.05)
+    except:
+        next
+
+print(covoit_v1)
